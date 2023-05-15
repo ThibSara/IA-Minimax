189dfd77699a7e3974ef25005b70b14c1eace020
Ruben-adhesif <rubencapp@gmail.com>
2023-05-15 09:06:39 +0200

    MinMax Utimate fini mais pas teste

--- Board.py
+++ Board.py
@@ -1,17 +1,4 @@
-import random
-
-# Définition de la classe Board
-
-
-
-# liste des tuples de toutes les combinaisons gagnantes rangé par ordre croissant
-winning_combination = (
-    (0,1,2),(3,4,5),(6,7,8),
-    (0,3,6),(1,4,7),(2,5,8),
-    (0,4,8),(2,4,6))
-
 class Board:
-    #definition d'un tableau de tic tac toe
     def __init__(self, board = None):
         if board is None:
             self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -19,12 +6,10 @@ class Board:
             self.board = board
         self.winner = 0
 
-    # impression du tableau du tic tac toe
     def __str__ (self):
         out = []
         for i in range(9):
             if (i%3 == 0) : out += "\n"
-        # on remplace les valeurs du tableau par des X et des O pour un effet visuel
             if (self.board[i]==0):
                 out += ' '
             elif (self.board[i]==1):
@@ -33,11 +18,15 @@ class Board:
                 out+='O'
         return (" | ".join(out)+' | ')
 
-    # retour le nombre de nombre de pair et singleton potentiellement gagnant
-    # pair vaut 3 fois plus que singleton pour pousser à jouer au deuxième coups
+    # Trie les combinaisons potentiellement gagnant et retroune :
+    #   +1pt pour chaque jeton potentiellement gagnant
+    #   +3pt pour chaque pair potentiellement gagnant (ratio x3 jeton pour pousser à jouer un 2eme coups)
+    #   +9pt pour chaque trio gagnant (ratio trouvé de façon itérative)
+    # Une combinaison est considéré potentiellement gagant si : 
+    #   -tout est élement sont compris dans l'un des winning_trio de winning_combinaison
+    #   -ET si les autres cases du winning_trio sont vides
     def Evaluate(self, joueur):
         cpt = 0
-        # utile pour la fonction evaluate, score à changer
         for trio in self.SetJoueur(joueur, n=3):
             for winning in winning_combination:
                 if trio[0] in winning and trio[1] in winning and trio[2] in winning:
@@ -45,22 +34,21 @@ class Board:
 
         for pair in self.SetJoueur(joueur, n=2):
             for trio in winning_combination:
-                #if (element de pair dans trio AND le troisème element de trio vide) : cpt +=3
+                #if (chaque element de pair dans trio) AND le troisème element du trio vide
                 if pair[0] in trio and pair[1] in trio and self.board[trio[0]] * self.board[trio[1]] * self.board[trio[2]] == 0 :
-                    #print(str(pair)+" in "+ str(trio))
                     cpt += 3
     
-        for elem in self.SetJoueur(joueur, n=1):
+        for jeton in self.SetJoueur(joueur, n=1):
             for trio in winning_combination:
-                #if (singleton dans trio AND les 2 autres elements de trio vide) : cpt +=1
-                if elem in trio and ( 
+                #if (jeton dans trio) AND (les 2 autres elements de trio vide)
+                if jeton in trio and ( 
                         self.board[trio[0]] + self.board[trio[1]] == 0 or
                         self.board[trio[0]] + self.board[trio[2]] == 0 or
                         self.board[trio[1]] + self.board[trio[2]] == 0 ):
-                    #print(str(elem)+" in "+ str(trio))
                     cpt += 1 
         return cpt 
-
+    
+    # Verifie si la partie est fini et initialise dans ce cas le parametre winner
     def TerminalTest(self):
         for i in winning_combination:
             if i in self.SetJoueur(1, 3):
@@ -70,63 +58,70 @@ class Board:
                 self.winner = 2
                 return 2
         return 0
+    
 
-    # crée une liste de toutes les combinaison du joueur de n combinaisons
-    def SetJoueur(self, joueur, n = 3):
-        # recupère les position de tout les jeton du joueur
-        idx = [i for i in range(9) if self.board[i] == joueur]
-    
-        # si le joueur n'a pas posé assez de combinaisons pour pouvoir gagner
-        if len(idx) < n: 
-            return ()       
-    
-        # cree un tuple pour tout les set d'indexe possible
-        out = []
-        if n == 3:
-            for i in range(len(idx)-2):
-                for j in range(1,len(idx)-i-1):
-                    out.append((idx[i], idx[i+j], idx[i+j+1]))
-        elif n == 2:
-            for i in range(len(idx)-1):
-                for j in range(1,len(idx)-i):
-                    out.append((idx[i], idx[i+j]))
-        else:
-            out = idx
-        return tuple(out)
-    
-      
+# Récupère la liste de tout les jetons du joueur placé en parametre
+# Renvoie toutes les combinaisons possible de n-éléments de cette liste
+def SetJoueur(board, joueur, n = 3):
+    # recupère les position de tout les jeton du joueur
+    idx = [i for i in range(9) if board[i] == joueur]
 
-# Pas dans class pour ne pas modifier le réel board
-def Minimax(game, alpha, beta, joueur, isPlaying = True):
+    # si le joueur n'a pas posé assez de combinaisons pour pouvoir gagner
+    if len(idx) < n: 
+        return ()       
+
+    # cree un tuple pour tout les set d'indexe possible
+    out = []
+    if n == 3:
+        for i in range(len(idx)-2):
+            for j in range(1,len(idx)-i-1):
+                out.append((idx[i], idx[i+j], idx[i+j+1]))
+    elif n == 2:
+        for i in range(len(idx)-1):
+            for j in range(1,len(idx)-i):
+                out.append((idx[i], idx[i+j]))
+    else:
+        out = idx
+    return tuple(out)
+
+# Calcul de l'interet à joueur sur une case des cases du board
+# Fonction mise en dehors du boar pour éviter toute moification du véritable board
+def Minimax(game : Board, alpha, beta, joueur, isPlaying = True):
     #retourne la valeur de la grille si la partie est finie
     if game.TerminalTest != 0:
-        return (game.board.Evaluate(), -1)
+        return game.Evaluate(game.winner)
     
     if isPlaying:
         best_score  = float('-inf')
-        for i in range(9):
-            if game.board[i] == 0:
-                game.board[i] = joueur
-                (score,idx) = game.Minimax(game, alpha, beta, joueur, False)
-                best_score = max(score,best_score)
+        for idx in range(9):
+            if game.board[idx] == 0:
+                game.board[idx] = joueur
+                score = Minimax(game, alpha, beta, joueur, False) 
+                best_score = max(best_score,score)
                 alpha = max(alpha,best_score)
                 if beta <= alpha:
                     break
-        return (best_score,i)   #juste besoin du dernier idx pour savoir où jouer
-
+        return best_score
     else:
         best_score=float('inf')
-        for i in range(9):
-            if game.board[i] == 0:
-                game.board[i] = Adversaire(joueur)
-                (score,idx) = game.Minimax(game, alpha, beta, joueur, True)
-                best_score = min(score, best_score)
+        for idx in range(9):
+            if game.board[idx] == 0:
+                game.board[idx] = Adversaire(joueur)
+                score = Minimax(game, alpha, beta, joueur, True)
+                best_score = min(best_score,score)
                 beta = min(beta,best_score)
                 if beta<=alpha:
                     break
-        return (best_score,i)
-    
+        return best_score
+
+# Renvoie le nombre de l'adversaire du joueur en parametre
 def Adversaire(joueur):
     if joueur == 1:
         return 2
     return 1
+
+# Liste de toutes les combinaisons gagnantes rangé par ordre croissant
+winning_combination = (
+    (0,1,2),(3,4,5),(6,7,8),
+    (0,3,6),(1,4,7),(2,5,8),
+    (0,4,8),(2,4,6))
--- Ultimate.py
+++ Ultimate.py
@@ -1,53 +1,79 @@
-import Board
-
-# Définition de la classe UltimateBoard
+from Board import Board, Adversaire
 
 class UltimateBoard:
     def __init__(self):
-        self.board_list = [Board() for i in range(9)]
+        self.list = [Board() for i in range(9)]
         self.ulti_board = Board()
         
     def __str__(self):
         for i in range(0, 9, 3):
             if i % 3 == 0:
                 print(" ----------------------------------------------")
-            rows = [str(ub).split("\n") for ub in self.board_list[i:i+3]]
+            rows = [str(ub).split("\n") for ub in self.list[i:i+3]]
             for j in range(len(rows[0])):
                 row_str = "-".join([row[j] for row in rows])
                 print(row_str)
     
-    def CheckUltimate(self):
+    def TerminalTest(self):
         for i in range(9):
-            self.ulti_board[i] = self.board_list[i].winner
+            self.ulti_board[i] = self.list[i].TerminalTest
+        return self.ulti_board.TerminalTest
 
-
-def Minimax(game, alpha, beta, joueur, isPlaying = True):
-    #retourne la valeur de la grille si la partie est finie
-    if game.ulti_board.TerminalTest != 0:
-        return (game.board.Evaluate(), -1)
+# Applique le MinMaxBoard au board joué (indiqué par idxBoard)
+# Lorsque l'on est Max et qu'un jeton est placé, il calcul le Min uniquement sur la board où il est renvoyé (et inversement pour Min)
+def Minmax(game : UltimateBoard, idxboard : int, alpha, beta, joueur, isPlaying = True):
+    #retourne la valeur de la grille si la partie est finie    
+    if game.TerminalTest != 0:
+        return game.list[idxboard].Evaluate(game.ulti_board.winner)
     
+    # si personne n'a encore gagner le board, le board est encore active
+    if game.list[idxboard].winner == 0:
+        board = game.list[idxboard]
+        return MinmaxBoard(board, alpha, beta, joueur, isPlaying)
+    
+    # sinon prendre en compte les differents board active dans l'arborescence minmax
+    elif isPlaying:
+        for idx in range(9):
+            if game.list[idx].winner == 0:
+                score = MinmaxBoard(game, idx, alpha, beta, joueur, True)
+                best_score = max(best_score,score)
+                alpha = max(alpha,best_score)
+                if beta <= alpha:
+                    break       #possible erreur : est ce que le break stop bien le for 
+        return best_score
+    else :
+        for idx in range(9):
+            if game.list[idx].winner == 0:
+                score = MinmaxBoard(game, idx, alpha, beta, joueur, False)
+                best_score = min(best_score,score)
+                beta = min(beta,best_score)
+                if beta <= alpha:
+                    break
+        return best_score
+
+# Version alternative de Board.Minmax en remplacant "game.board" par "game.list[idxBoard]"
+# Mais l'indice du jeton posé (idx) remplace l'indice de la porchaine board (idxBoard)
+def MinmaxBoard(game : UltimateBoard, idxBoard : int, alpha, beta, joueur, isPlaying):
     if isPlaying:
         best_score  = float('-inf')
-        for i in range(9):
-            if game.board[i] == 0:
-                game.board[i] = joueur
-                (score,idx) = game.Minimax(game, alpha, beta, joueur, False)
-                best_score = max(score,best_score)
+        for idx in range(9):
+            if game.list[idxBoard][idx] == 0:
+                game.list[idxBoard][idx] = joueur
+                score = Minmax(game, idx, alpha, beta, joueur, False) 
+                best_score = max(best_score,score)
                 alpha = max(alpha,best_score)
                 if beta <= alpha:
                     break
-        return (best_score,i)   #juste besoin du dernier idx pour savoir où jouer
-
+        return best_score
+    
     else:
         best_score=float('inf')
-        for i in range(9):
-            if game.board[i] == 0:
-                game.board[i] = Board.Adversaire(joueur)
-                (score,idx) = game.Minimax(game, alpha, beta, joueur, True)
-                best_score = min(score, best_score)
+        for idx in range(9):
+            if game.list[idxBoard][idx] == 0:
+                game.list[idxBoard][idx] = Adversaire(joueur)
+                score = Minmax(game, idx, alpha, beta, joueur, True)
+                best_score = min(best_score,score)
                 beta = min(beta,best_score)
                 if beta<=alpha:
                     break
-        return (best_score,i)
-
-
+        return best_score
